892f241fdf731660e2c4f69721e88596bdd58115
Compeee <eero.kaarnalehto@outlook.com>
2021-11-17 17:56:00 +0200

    heroku

--- server.js
+++ server.js
@@ -7,10 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+const routes = require("./backend/routes/index");
 
-app.use("/api", require("./routes/index"));
+app.use("/api", require("./backend/routes/index"));
 
 app.use(cors());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
