67e3e692cd9deaaacf6ae405bdd6fb9ba8fa3f8d
Compeee <eero.kaarnalehto@outlook.com>
2021-11-17 19:16:07 +0200

    tweak to index.js

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
-
 app.use("/api", require("./routes/index"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+}
 
 app.use(cors());
 
