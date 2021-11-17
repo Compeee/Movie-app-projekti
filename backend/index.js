9c56a3c72647bddab5f1a5cd56c74353a5673a1b
Compeee <eero.kaarnalehto@outlook.com>
2021-11-17 18:07:51 +0200

    Nothing new, jst a re

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const routes = require("./backend/routes/index");
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+app.use("/api", require("./routes/index"));
 
 app.use(cors());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
-}
-app.use("/api", routes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
