494456b78262f0e4e02e2610bef691acf1589168
Compeee <eero.kaarnalehto@outlook.com>
2021-11-17 19:19:01 +0200

    .

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ const app = express();
 app.use("/api", require("./routes/index"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "frontend/public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend/public", "index.html"));
   });
 }
 
