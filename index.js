b823321c7e1c77e8b2e5ce082960d6a9437d0d7a
Compeee <eero.kaarnalehto@outlook.com>
2021-11-18 21:15:00 +0200

    redeploy heroku

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use("/api", require("./routes/index"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/public")));
-
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend/public", "index.html"));
   });
