e35689e1d684af1a9ca1e69b8201ef7df99e02b0
Compeee <eero.kaarnalehto@outlook.com>
2021-11-18 21:10:27 +0200

    component to show upcoming movies and api route for it

--- frontend/src/Components/Upcoming.js
+++ frontend/src/Components/Upcoming.js
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import Movies from "./Movies";
+import ReactPaginate from "react-paginate";
+import { useState, useEffect } from "react";
+import axios from "axios";
+// HomePage displays the most popular shows and movies
 export default function Upcoming() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const pages = 10;
+  const [movies, setMovies] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  useEffect(() => {
+    // Gets upcoming movies
+    axios.get(`/api/upcoming?page=${pageNum}`).then((res) => {
+      setMovies(res.data.results);
+      console.log(res.data.results);
+    });
+  }, [pageNum]);
+
+  const handlePageClick = (data) => {
+    setPageNum(data.selected + 1);
+  };
+  return (
+    <div>
+      <div className="col">
+        {" "}
+        <h3>Movies</h3>
+      </div>
+      <Movies movies={movies} />
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel="Next"
+        onPageChange={handlePageClick}
+        pageCount={pages}
+        marginPagesDisplayed={3}
+        pageRangeDisplayed={5}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+      />
+    </div>
+  );
 }
--- routes/index.js
+++ routes/index.js
@@ -82,5 +82,23 @@ router.get("/search", async (req, res) => {
     res.status(500).json({ error });
   }
 });
+router.get("/upcoming", async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    });
 
+    const apiRes = await needle(
+      "get",
+      `${API_BASE_URL}` +
+        "/movie/upcoming?sort_by=popularity.desc&" +
+        `${params}`
+    );
+    const data = apiRes.body;
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 module.exports = router;
