528f60aedd4bcb5c155c9124c5338651f4ecf228
Compeee <eero.kaarnalehto@outlook.com>
2021-11-19 21:30:44 +0200

    Made footer sticky

--- frontend/src/Footer/Footer.js
+++ frontend/src/Footer/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import "./Footer.css";
 export default function Footer() {
   return (
-    <footer class="bg-dark text-center text-white">
-      <div class="text-center p-3">©</div>
+    <footer class="bg-dark text-center text-white fixed-bottom">
+      <div class="container">
+        <div class="text-center p-3">©</div>
+      </div>
     </footer>
   );
 }
